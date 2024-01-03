a2e48ecfc33c6361d08df83f7e2a6f215653c6e5
hanyongjun <gksdydwns34@naver.com>
2024-01-03 20:56:02 +0900

    sns 로그인 디자인 추가

--- src/main/front_end/travelogue_front/src/assets/components/LoginPage.vue
+++ src/main/front_end/travelogue_front/src/assets/components/LoginPage.vue
@@ -20,14 +20,30 @@
             <input type="text" class="input-field" placeholder="이름" name="userName" maxlength="20">
             <button class="submit">회원가입</button>
         </form>
+        
+        <div class="sns_text_underline">
+          <p> 다른 계정으로 로그인 </p>
+       </div>
 
         <div class="sns_login">
-          <button id="kakao"><img src="../img/kakao_login_medium.png"></button>
-          <button id="google"><img src="../img/googlelogin.png"></button>
-          <button><img src="../img/kakao_login_medium.png"></button>
+          <button id="google">
+            <img src="../img/google.png" id="google_img">
+          </button>
+
+          <button id="kakao">
+            <img src="../img/kakao.png" id="kakao_img">
+          </button>
+                   
+          <button id="naver">
+            <img src="../img/naver.png" id="naver_img">
+          </button>
         </div>
 
-       <div class="image"><img src="../img/hanok.webp"></div> 
+
+       <div class="image">
+        <img src="../img/hanok.webp">
+      </div>
+      
    
       </div>
   </div>
